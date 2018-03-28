2cc7eb2766563dccc0aedb1acdfd7743b32808f8
Piumika Welagedara <piumika@Piumikas-MacBook-Pro.local>
2018-03-28 23:42:40 +1100

    Created initial project structure

---
+++ IOSProficiencyExercise/IOSProficiencyExercise/FactsTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FactsTableViewController.swift
+//  IOSProficiencyExercise
+//
+//  Created by Piumika Welagedara on 28/3/18.
+//  Copyright © 2018 Piumika Welagedara. All rights reserved.
+//
+
+import UIKit
+
+class FactsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
